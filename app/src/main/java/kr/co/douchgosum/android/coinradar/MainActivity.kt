95cddb7b13a53609b11d6f489b3df5abbd58b748
unnamedw <csw284891@naver.com>
2020-10-15 03:24:01 +0900

    모든 거래소 Ticker Api 작성 완료

--- app/src/main/java/kr/co/douchgosum/android/coinradar/MainActivity.kt
+++ app/src/main/java/kr/co/douchgosum/android/coinradar/MainActivity.kt
@@ -1,12 +1,89 @@
 package kr.co.douchgosum.android.coinradar
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var mInterstitialAd: InterstitialAd
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.CustomAppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+
+        //전면광고 표시
+//        showAd(R.string.admob_interstitialad_id)
     }
+
+    private fun initView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(bottom_nav.menu)
+        bottom_nav
+            .setupWithNavController(navController)
+    }
+
+    override fun onBackPressed() {
+        val count = supportFragmentManager.backStackEntryCount
+        if (count == 0) {
+            super.onBackPressed()
+        } else {
+            supportFragmentManager.popBackStack()
+        }
+    }
+
+    //광고표시
+    private fun showAd(adId: Int) {
+        val adResource = resources.getString(adId)
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = adResource
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+        mInterstitialAd.adListener = object : AdListener() {
+            override fun onAdLoaded() {
+                if (mInterstitialAd.isLoaded) {
+                    mInterstitialAd.show()
+                } else {
+                    Log.d("MyTag", "The interstitial wasn't loaded yet.")
+                }
+            }
+        }
+    }
+
+    //리모트 컨픽 초기화
+    private fun initRemoteConfig() {
+        val remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+        remoteConfig.fetchAndActivate().addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                val updated = task.result
+                Log.d("MyTag", "Config params updated: $updated")
+                Log.d("MyTag", "fetch succeed")
+                val string = remoteConfig.getString("lottery_data")
+                Log.d("MyTag", "총 글자수: ${string.length}")
+            } else {
+                Log.d("MyTag", "fetch failed")
+            }
+        }
+    }
+
+
 }
